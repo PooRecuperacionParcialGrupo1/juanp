7300810c14f590c874347004fff45af779272589
jperesse <jperesse@unal.edu.co>
2026-03-23 15:27:39 -0500

    version 6.0
    creamos clase podcast

--- PPPOOGRUPO1/src/type2/Pelicula.java
+++ PPPOOGRUPO1/src/type2/Pelicula.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package type2;
+
 
 /**
  *
- * @author sebas
+ * @author jperesse
  */
-public class Pelicula {
-    
+
+package type2;
+
+public class Pelicula extends contenidoMultimedia {
+    String director;
+
+    public Pelicula(String titulo, int duracionMinutos, int anioLanzamiento, String director) {
+        super(titulo, duracionMinutos, anioLanzamiento);
+        this.director = director;
+    }
+
+    @Override
+    void iniciarReproduccion() {
+        System.out.println("Cargando video y ajustando subtítulos para la película...");
+    }
+
+    void configurarVisualizacion() {
+        System.out.println("Reproduciendo en calidad Automática, idioma original");
+    }
+
+    void configurarVisualizacion(String calidad) {
+        System.out.println("Reproduciendo en calidad " + calidad);
+    }
+
+    void configurarVisualizacion(String calidad, String idiomaSubtitulos) {
+        System.out.println("Reproduciendo en calidad " + calidad + " con subtítulos en " + idiomaSubtitulos);
+    }
 }
