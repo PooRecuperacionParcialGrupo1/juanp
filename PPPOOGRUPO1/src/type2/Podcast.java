f12cd7bb6f5a202c0d4eafdcfee6a3230c0e5b04
jperesse <jperesse@unal.edu.co>
2026-03-23 20:44:58 -0500

    version 9.0
    cambios para las clases clase contenidoMUltimedia presenta grises incluso borrrando y creando de nuevo

--- PPPOOGRUPO1/src/type2/Podcast.java
+++ PPPOOGRUPO1/src/type2/Podcast.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package type2;
 
 public class Podcast extends contenidoMultimedia {
@@ -17,3 +13,5 @@ public class Podcast extends contenidoMultimedia {
         System.out.println("Conectando al servidor de audio y preparando el episodio del podcast...");
     }
 }
+
+
--- PPPOOGRUPO1/src/type2/contenidomultimedia.java
+++ PPPOOGRUPO1/src/type2/contenidomultimedia.java
@@ -1,10 +1,5 @@
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package type2;
 
 public abstract class contenidoMultimedia {
